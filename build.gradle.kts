2a1b481bd5b788afca761f285ad12005e95e6295
Nicholas Christopher <nwillc@gmail.com>
2019-08-04 22:27:52 -0400

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.1.6.RELEASE"
+	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	kotlin("jvm") version "1.3.41"
+	kotlin("plugin.spring") version "1.3.41"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.liquibase:liquibase-core")
+
+	runtime("org.postgresql:postgresql:42.2.6")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	testRuntime("com.h2database:h2")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
