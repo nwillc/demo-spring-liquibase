a519b0d2040a827d2c744f83a44c7fd37c775c95
Nicholas Christopher <nwillc@gmail.com>
2019-08-07 00:08:35 -0400

    testing numbers

--- src/main/kotlin/com/example/demo/Numbers.kt
+++ src/main/kotlin/com/example/demo/Numbers.kt
@@ -2,11 +2,13 @@ package com.example.demo
 
 import kotlin.random.Random
 
-const val LIMIT = 10
+
 data class Numbers(
     val name: String,
     val draw1: Int = Random.nextInt(1, LIMIT),
-    val draw2: Int = Random.nextInt(1, LIMIT + 1 - draw1)
+    val draw2: Int = Random.nextInt(1, LIMIT - draw1 + 1)
 ) {
-
+    companion object {
+        const val LIMIT = 50
+    }
 }
--- src/test/kotlin/com/example/demo/NumbersTest.kt
+++ src/test/kotlin/com/example/demo/NumbersTest.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class NumbersTest {
@@ -8,6 +9,7 @@ class NumbersTest {
     fun `should be able to create a numbers instance`() {
         for (i in 1 .. 100) {
             val numbers = Numbers("Foo")
+            assertThat(numbers.draw1 + numbers.draw2).isLessThanOrEqualTo(Numbers.LIMIT)
             println(numbers)
         }
     }
