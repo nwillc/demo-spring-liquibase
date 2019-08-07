9e1225df584fa875578e2dd831700f8ffa8a34ad
Nicholas Christopher <nwillc@gmail.com>
2019-08-07 00:18:11 -0400

    tests

--- src/test/kotlin/com/example/demo/NumbersTest.kt
+++ src/test/kotlin/com/example/demo/NumbersTest.kt
@@ -7,8 +7,8 @@ class NumbersTest {
 
     @Test
     fun `should be able to create a numbers instance`() {
-        for (i in 1 .. 100) {
-            val numbers = Numbers("Foo")
+        for (i in 1 .. Numbers.PENNIES * Numbers.PENNIES) {
+            val numbers = Numbers("$i")
             assertThat(numbers.pennies1).isBetween(1, Numbers.PENNIES - 1)
             assertThat(numbers.pennies2).isBetween(1, Numbers.PENNIES - 1)
             assertThat(numbers.pennies1 + numbers.pennies2).isLessThanOrEqualTo(Numbers.PENNIES)
