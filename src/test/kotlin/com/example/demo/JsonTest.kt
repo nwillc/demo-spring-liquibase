a9b822b044cb07d36b0033a43a0255d924670695
Nicholas Christopher <nwillc@gmail.com>
2019-08-08 22:32:52 -0400

    json tests

--- src/test/kotlin/com/example/demo/JsonTest.kt
+++ src/test/kotlin/com/example/demo/JsonTest.kt
@@ -1,9 +1,16 @@
 package com.example.demo
 
+import com.beust.klaxon.Converter
+import com.beust.klaxon.JsonValue
 import com.beust.klaxon.Klaxon
 import com.google.gson.Gson
 import org.junit.jupiter.api.Test
 import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.Status
+
+internal val klaxon: Klaxon = Klaxon()
+    .converter(HealthConverter)
+    .converter(StatusConverter)
 
 class JsonTest {
     @Test
@@ -11,6 +18,34 @@ class JsonTest {
         val health = Health.up().build()
         println(health)
         println(Gson().toJson(health))
-        println(Klaxon().toJsonString(health))
+        println(klaxon.toJsonString(health))
+    }
+}
+
+internal object StatusConverter : Converter {
+    override fun canConvert(cls: Class<*>) = cls == Status::class.java
+
+    override fun fromJson(jv: JsonValue): Any? {
+        TODO("not implemented")
+    }
+
+    override fun toJson(value: Any): String {
+        value as Status
+        return """
+            {"code":"${value.code}","description":"${value.description}"}
+        """.trimIndent()
+    }
+}
+
+internal object HealthConverter : Converter {
+    override fun canConvert(cls: Class<*>) = cls == Health::class.java
+
+    override fun fromJson(jv: JsonValue): Any? {
+        TODO("not implemented")
+    }
+
+    override fun toJson(value: Any): String {
+        value as Health
+        return """{"status":${klaxon.toJsonString(value.status)},"details":${klaxon.toJsonString(value.details)}}"""
     }
 }
